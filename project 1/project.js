c78e760f5b0a6b168b91472fc7702ffd3e55b971
ivan <vanyakorebyk@gmail.com>
2020-02-20 20:22:46 +0200

    update

--- project 1/project.js
+++ project 1/project.js
@@ -1,6 +1,10 @@
-'use strict'
+"use strict";
 
-let money = +prompt("Ваш бюджет на місяць?", "200");
+do {
+    var money = parseInt(prompt("Ваш бюджет на місяць?", "200"));
+    console.log(money);
+}while(isNaN(money) || money == 0);
+
 let time = prompt("Введіть дату в форматі YYYY-MM-DD", "1999-12-09");
 
 let appData = {
@@ -15,14 +19,20 @@ let appData = {
     optionalExpenses: {},
     income: [],
     saving: false
-}
+};
 
 appData.budget = money; //Внесення бюджету в об'єкт
 appData.timeData = time; //Внесення дати в об'єкт
 
 appData.expenses.firstBudQue = prompt("Введіть обов'язкову статтю витрат у цьому місяці");
-appData.expenses.firstCountQue = +prompt("У скільки обійдеться");
+do {
+appData.expenses.firstCountQue = parseInt(prompt("У скільки обійдеться"));
+} while (isNaN(appData.expenses.firstCountQue) || appData.expenses.firstCountQue == 0);
+
 appData.expenses.secondBudQue = prompt("Введіть другу обов'язкову статтю витрат у цьому місяці");
+
+do {
 appData.expenses.secondCountQue = +prompt("У скільки обійдеться");
+} while (isNaN(appData.expenses.secondCountQue) || appData.expenses.secondCountQue == 0);
 
 alert("Бюджет на день " + " " + (appData.budget - (appData.expenses.firstCountQue +appData.expenses.secondCountQue)) / 30);
