f937ecc01ce840a65496308364b562da69185e75
Ivan <vanyakorebryk@gmail.com>
2020-02-20 20:56:37 +0200

    Покращив код програми

--- project 1/project.js
+++ project 1/project.js
@@ -3,36 +3,43 @@
 do {
     var money = parseInt(prompt("Ваш бюджет на місяць?", "200"));
     console.log(money);
-}while(isNaN(money) || money == 0);
+} while (isNaN(money) || money == 0);
 
 let time = prompt("Введіть дату в форматі YYYY-MM-DD", "1999-12-09");
 
 let appData = {
-    budget: null,
-    timeData: null,
-    expenses: {
-        firstBudQue: null,
-        secondBudQue: null,
-        firstCountQue: null,
-        secondCountQue: null
-    },
+    budget: money, //Внесення значення
+    timeData: time, //внесення значення
+    expenses: {},
     optionalExpenses: {},
     income: [],
     saving: false
 };
 
-appData.budget = money; //Внесення бюджету в об'єкт
-appData.timeData = time; //Внесення дати в об'єкт
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введіть обов'язкову статтю витрат"),
+    b = prompt("У скільки обійдеться");
+    
+    if (typeof(a) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+    && a != "" && b != "" && a.length < 50) {
+    console.log("done");
+    appData.expenses[a] = b;
+    } else {
+    alert("Спробуйте ще раз");
+    i = 0;
+    }
+}
 
-appData.expenses.firstBudQue = prompt("Введіть обов'язкову статтю витрат у цьому місяці");
-do {
-appData.expenses.firstCountQue = parseInt(prompt("У скільки обійдеться"));
-} while (isNaN(appData.expenses.firstCountQue) || appData.expenses.firstCountQue == 0);
+appData.moneyPerday = appData.budget / 30;
 
-appData.expenses.secondBudQue = prompt("Введіть другу обов'язкову статтю витрат у цьому місяці");
+alert("Щоденний бюджет: " + appData.moneyPerday);
 
-do {
-appData.expenses.secondCountQue = +prompt("У скільки обійдеться");
-} while (isNaN(appData.expenses.secondCountQue) || appData.expenses.secondCountQue == 0);
-
-alert("Бюджет на день " + " " + (appData.budget - (appData.expenses.firstCountQue +appData.expenses.secondCountQue)) / 30);
+if (appData.moneyPerday < 100) {
+    console.log("Мінімальний рівень достатку");
+} else if (appData.moneyPerday > 100 && appData.moneyPerday < 2000) {
+    console.log("Середній рівень достатку");
+} else if (appData.moneyPerday > 2000) {
+    console.log("Високий рівень достатку");
+} else {
+    сonsole.log("Сталася помилка");
+}
